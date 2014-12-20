28f80154280a70629761f0b008db0031c7d83aa1
Marijn van der Zee <marijn@serraict.com>
2014-12-20 14:05:02 +0100

    Start of Jenkins support, for #331
    A simple copy of `TeamCity` and `TeamCityTests`; rename to Jenkins only

---
+++ GitVersionCore/BuildServers/Jenkins.cs
@@ -0,0 +1,43 @@
+﻿namespace GitVersion
+{
+    using System;
+
+    public class Jenkins : BuildServerBase
+    {
+        Authentication authentication;
+
+        public Jenkins(Authentication authentication)
+        {
+            this.authentication = authentication;
+        }
+
+        public override bool CanApplyToCurrentContext()
+        {
+            return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("TEAMCITY_VERSION"));
+        }
+
+        public override void PerformPreProcessingSteps(string gitDirectory)
+        {
+            if (string.IsNullOrEmpty(gitDirectory))
+            {
+                throw new WarningException("Failed to find .git directory on agent. Please make sure agent checkout mode is enabled for you VCS roots - http://confluence.jetbrains.com/display/TCD8/VCS+Checkout+Mode");
+            }
+
+            GitHelper.NormalizeGitDirectory(gitDirectory, authentication);
+        }
+
+        public override string[] GenerateSetParameterMessage(string name, string value)
+        {
+            return new[]
+            {
+                string.Format("##teamcity[setParameter name='GitVersion.{0}' value='{1}']", name, ServiceMessageEscapeHelper.EscapeValue(value)),
+                string.Format("##teamcity[setParameter name='system.GitVersion.{0}' value='{1}']", name, ServiceMessageEscapeHelper.EscapeValue(value))
+            };
+        }
+
+        public override string GenerateSetVersionMessage(string versionToUseForBuildNumber)
+        {
+            return string.Format("##teamcity[buildNumber '{0}']", ServiceMessageEscapeHelper.EscapeValue(versionToUseForBuildNumber));
+        }
+    }
+}
